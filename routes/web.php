1f248d454910b5fb328cc834a99fa3fef622708b
gretar <gretar95@gmail.com>
2017-09-14 13:04:07 +0000

    using threads controller

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,14 @@
 
 Auth::routes();
 
-Route::get("/threads", function() {
-	return view("threads.index");
-});
+Route::get("/threads", "ThreadsController@index");
 
-Route::get("/threads/create", function() {
-	return view("threads.create");
-});
+Route::get("/threads/create", "ThreadsController@show");
 
-Route::get("/", function() {
-	$name = "Gretar";
-	return view("messages.hello", compact("name"));
-});
-
+Route::get("/threads/{id}", "ThreadsController@show");
 
 
 /*Route::get("/greeting/{id}", function($id){
 	$greetings = ["hola", "hello", "hallo", "hi", "hey"];
 	return view("messages.greeting", ["greeting" => $greetings[$id]]);
 })->where(["id" => "[0-4]"]);*/
-
-Route::get("/", "GreetingsController@show");
