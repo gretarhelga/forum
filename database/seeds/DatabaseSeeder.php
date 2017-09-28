fee9a78b5f376d518ba2ae4bde4152440de0c4e1
gretar <gretar95@gmail.com>
2017-09-28 15:04:39 +0000

    timaverk8

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -25,5 +25,22 @@ class DatabaseSeeder extends Seeder
             ["title" => "hi amigo", "body" => "hi amiog hvern er du?", "user_id" => 1],
             ["title" => "hello amigo", "body" => "hello amiog hvern er du?", "user_id" => 1]
         ]);
+
+
+        DB::table('leikstjori')->insert([
+            ['nafn' => "Gretar",'thjoderni' => "island"],
+            ['nafn' => "Sammi",'thjoderni' => "island"],
+            ['nafn' => "Baldur",'thjoderni' => "island"],
+            ['nafn' => "Dimitri",'thjoderni' => "russland"]
+        ]);
+
+        DB::table("dvd")->insert([
+            ["titill" => "Batman", "adalleikari" => "Ben", "utgafuar" => 1995, "verd" => 1000, "flokkur" => "spennu", "leikstjori_id" => 1],
+            ["titill" => "Superman", "adalleikari" => "Jones", "utgafuar" => 1996, "verd" => 2500, "flokkur" => "Hryllings", "leikstjori_id" => 2],
+            ["titill" => "Spiderman", "adalleikari" => "Hillary", "utgafuar" => 1937, "verd" => 3697, "flokkur" => "Hasar", "leikstjori_id" => 3],
+            ["titill" => "Spy kids", "adalleikari" => "Sam", "utgafuar" => 2006, "verd" => 6559, "flokkur" => "Drama", "leikstjori_id" => 4],
+            ["titill" => "X men", "adalleikari" => "Damion", "utgafuar" => 1967, "verd" => 7895, "flokkur" => "spennu", "leikstjori_id" => 2],
+            ["titill" => "Harry Potter", "adalleikari" => "Santa", "utgafuar" => 1879, "verd" => 1563, "flokkur" => "Hasar", "leikstjori_id" => 3],
+        ]);
     }
 }
