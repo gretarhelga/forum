c12835095cab9935bf22a37928073088e930ed61
gretar <gretar95@gmail.com>
2017-10-02 15:10:49 +0000

    eloquent

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Thread;
 
 class ThreadsController extends Controller
 {
     public function index()
     {
-        $threads = DB::table("threads")->get();
+        $threads = Thread::all();
 
     	return view("threads.index", compact("threads"));
     }
@@ -23,8 +24,8 @@ class ThreadsController extends Controller
 
     public function show($id)
     {
-        $thread = DB::table("threads")->where("id", $id)->first();
-
+        $thread = Thread::find($id);
+        
     	return view("threads.show", compact("thread"));
     }
     public function store ()
